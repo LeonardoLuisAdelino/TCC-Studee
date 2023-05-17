8ac60625489f5795f8fd89151db3bdc8eb2e2910
Leonardo Luis Lopes Adelino <leonardo.adelino@grupomultiplica.com.br>
2023-05-17 10:09:14 -0300

    priemiro commit

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Importações necessárias
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Image } from 'react-native';
+import home from './screens/home';
+import Configuracoes from './screens/Configuracoes';
+import Perfil from './screens/Perfil';
+import Metodos from './screens/Metodos';
+import Tempo from './screens/Tempo';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: "black"
+          },
+          tabBarActiveTintColor: "blue",
+          tabBarInactiveTintColor: "grey"
+        }}
+      >
+        <Tab.Screen name='home' component={home} 
+          options={{
+            tabBarIcon: () => (
+              <Image
+                source={require('./icones/check.png')}
+                style={{ width: 24, height: 24 }}
+              />
+            ),
+          }}
+        />
+         <Tab.Screen name="metodos" component={Metodos}
+          options={{
+            tabBarIcon: () => (
+              <Image
+                source={require('./icones/book.png')}  
+                style={{height: 24, width: 24}}
+              />
+            )
+          }}
+        />
+        <Tab.Screen name="Ampulheta" component={Tempo}
+          options={{
+            tabBarIcon: () => (
+              <Image
+                source={require('./icones/hourglass.png')}  
+                style={{height: 24, width: 24}}
+              />
+            )
+          }}
+        />
+        <Tab.Screen name="Configurações" component={Configuracoes} 
+          options={{
+            tabBarIcon: () => (
+              <Image 
+                source={require('./icones/settings-gear-icon.png')}  
+                style={{width: 24, height: 24}}
+              />
+            )
+          }}
+        />
+        <Tab.Screen name="Perfil" component={Perfil} 
+          options={{
+            tabBarIcon: () => (
+              <Image
+                source={require('./icones/user.png')}  
+                style={{height: 24, width: 24}}
+              />
+            )
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
